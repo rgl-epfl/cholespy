81c406d641d8fc5d86d883a9c4c249f7ddb8ae0c
jeffquinn-msk <quinnj2@mskcc.org>
2023-12-22 16:34:34 -0500

    add enums

--- src/cholespy/__init__.py
+++ src/cholespy/__init__.py
@@ -1,3 +1,14 @@
 from importlib import import_module
 import_module('cholespy._cholespy_core')
 del import_module
+
+CHOLMOD_A = 0
+CHOLMOD_LDLt = 1
+CHOLMOD_LD = 2
+CHOLMOD_DLt = 3
+CHOLMOD_L = 4
+CHOLMOD_Lt = 5
+CHOLMOD_D = 6
+CHOLMOD_P = 7
+CHOLMOD_Pt = 8
+
